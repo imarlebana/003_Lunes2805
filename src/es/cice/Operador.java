ff73ee3d96a7a19ad58d2366c0f4c7ca822a33c1
imarlebana <imarlebana@gmail.com>
2018-05-28 19:58:58 +0200

    Commit: operadores

---
+++ src/es/cice/Operador.java
@@ -0,0 +1,27 @@
+package es.cice;
+
+public class Operador {
+
+    //Operadores de relacion ->, <; >;<=,>=, !=,==;
+    //Relacionan dos objetos o variables para conseguir true o false
+    static boolean r1 = 1 > 2;
+    static boolean r2 = 1 < 2;
+    static boolean r3 = 1 >= 1;
+    static boolean r4 = 1 <= 2;
+    static boolean r5 = 1 != 2;
+    static boolean r6 = 1 == 2;
+
+    //Operadores logicos && &; || |; !: ^
+
+    public static void main(String[] args) {
+
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+        System.out.println(r4);
+        System.out.println(r5);
+        System.out.println(r6);
+
+    }
+}
+
